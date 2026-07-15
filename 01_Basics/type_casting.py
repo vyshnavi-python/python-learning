04ce718a832ae71107b8f25422137d96dd11e858
Vyshnavi M <vyshnavimukiri@gmail.com>
2026-07-15 21:46:44 +0530

    Add student details and data type display

---
+++ 01_Basics/type_casting.py
@@ -0,0 +1,23 @@
+===== Student Details =====
+Enter your name: Vyshnavi
+Enter your age: 20
+Enter your college name: ABC College
+Enter your branch: CSE
+Enter your city: Hyderabad
+Enter your percentage: 92.5
+
+===== Student Information =====
+Name       : Vyshnavi
+Age        : 20
+College    : ABC College
+Branch     : CSE
+City       : Hyderabad
+Percentage : 92.5
+
+===== Data Types =====
+Name Type       : <class 'str'>
+Age Type        : <class 'int'>
+College Type    : <class 'str'>
+Branch Type     : <class 'str'>
+City Type       : <class 'str'>
+Percentage Type : <class 'float'>
