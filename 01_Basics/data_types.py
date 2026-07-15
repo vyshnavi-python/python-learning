08a0ce52619741a77d5575fa62b25c5bede6cd25
Vyshnavi M <vyshnavimukiri@gmail.com>
2026-07-15 21:09:29 +0530

    Add data types demonstration in data_types.py

---
+++ 01_Basics/data_types.py
@@ -0,0 +1,19 @@
+# Integer
+age = 20
+print("Age:", age)
+print(type(age))
+
+# Float
+percentage = 92.5
+print("Percentage:", percentage)
+print(type(percentage))
+
+# String
+name = "Vyshnavi"
+print("Name:", name)
+print(type(name))
+
+# Boolean
+is_student = True
+print("Is Student:", is_student)
+print(type(is_student))
