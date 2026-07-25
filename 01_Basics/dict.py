b1405f0e0919a1642b3ba57909c14ef7723b6ad0
Vyshnavi M <vyshnavimukiri@gmail.com>
2026-07-25 20:09:25 +0530

    Create dict.py

---
+++ 01_Basics/dict.py
@@ -0,0 +1,21 @@
+# ==========================================
+# Python Dictionaries - Complete Program
+# ==========================================
+
+# Creating Dictionary
+student = {
+    "name": "Vyshnavi",
+    "age": 20,
+    "branch": "CSE"
+}
+
+print("Original Dictionary:")
+print(student)
+
+# ------------------------------------------
+# Accessing Values
+# ------------------------------------------
+print("\nName:", student["name"])
+print("Age:", student["age"])
+
+# ------------------------------------------
