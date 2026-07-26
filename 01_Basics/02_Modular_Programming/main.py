88792de2794ad76460ded1a9ce68cc5f951f3dd7
Vyshnavi M <vyshnavimukiri@gmail.com>
2026-07-26 16:08:41 +0530

    Create main.py

---
+++ 01_Basics/02_Modular_Programming/main.py
@@ -0,0 +1,10 @@
+# ==========================================
+# Using User-defined Module
+# ==========================================
+
+import calculator
+
+print("Addition:", calculator.add(20, 10))
+print("Subtraction:", calculator.subtract(20, 10))
+print("Multiplication:", calculator.multiply(20, 10))
+print("Division:", calculator.divide(20, 10))
